4a2abd1631287ed668a89c5521a1d69de0e9faaa
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-04-30 14:40:38 +0200

    Incorporate SIRI v1.7 to site

--- build.sh
+++ build.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env bash
 
-# not perfect, but better than nothing
+# stop processing on basic errors
 set -e
 
+# NOTE: temporarily freezing to the latest tag,
+# ultimately something more flexible with be introduced
+# (e.g. multi-version support, and first "automatic latest version")
+SIRI_VERSION=v1.7
+TEMP_FOLDER=tmp
+CONTENT_FOLDER=content/normes
+SIRI_REPO_NAME=transport-profil-siri-fr
+
 echo "Retrieving content via git..."
-git submodule update --init --remote content/normes
+git submodule update --init --remote $CONTENT_FOLDER
 
 echo "Removing legacy SIRI content..."
-rm -rf content/normes/SIRI
+rm -rf $CONTENT_FOLDER/SIRI
+
+echo "Cloning new SIRI content..."
+rm -rf $TEMP_FOLDER | true
+mkdir $TEMP_FOLDER && cd $TEMP_FOLDER
+git clone git@github.com:etalab/$SIRI_REPO_NAME.git
+cd $SIRI_REPO_NAME
+# just for debugging purposes
+git tag -l
+git checkout tags/$SIRI_VERSION
+cd ../..
+
+echo "Copying new SIRI content to the right place..."
+cp -r $TEMP_FOLDER/$SIRI_REPO_NAME/SIRI $CONTENT_FOLDER
 
 echo "Building site..."
 hugo --minify
