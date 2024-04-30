b8e5dc92c4d028e6fc016546192073015e62f50b
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-04-30 12:16:17 +0200

    Remove legacy SIRI content

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ set -e
 echo "Retrieving content via git..."
 git submodule update --init --remote content/normes
 
+echo "Removing legacy SIRI content..."
+rm -rf content/normes/SIRI
+
 echo "Building site..."
 hugo --minify
