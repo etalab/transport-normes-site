401daaf7256f1e2624a061a73cf186b6a8d9d865
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-12-05 18:34:19 +0100

    Refactor build.sh to allow custom branch for NeTEx

--- build.sh
+++ build.sh
@@ -3,32 +3,33 @@
 # stop processing on basic errors
 set -e
 
-# NOTE: temporarily freezing to the latest tag,
-# ultimately something more flexible with be introduced
-# (e.g. multi-version support, and first "automatic latest version")
-SIRI_VERSION=v1.7
+NETEX_REPO_NAME=transport-profil-netex-fr
+NETEX_VERSION=order-sections
+SIRI_VERSION=tags/v1.7
+SIRI_REPO_NAME=transport-profil-siri-fr
 TEMP_FOLDER=tmp
 CONTENT_FOLDER=content/normes
-SIRI_REPO_NAME=transport-profil-siri-fr
 
-echo "Retrieving content via git..."
-git submodule update --init --remote $CONTENT_FOLDER
+function cloneContent() {
+    repository=$1
+    folder=$2
+    ref=$3
 
-echo "Removing legacy SIRI content..."
+    rm -rf $TEMP_FOLDER | true
+    mkdir $TEMP_FOLDER && cd $TEMP_FOLDER
+
+    git clone $repository
+    cd $folder
+    git checkout $ref
+    cd ../..
+}
+
+rm -rf $CONTENT_FOLDER/NeTEx
+cloneContent https://github.com/etalab/$NETEX_REPO_NAME.git $NETEX_REPO_NAME $NETEX_VERSION
+cp -r $TEMP_FOLDER/$NETEX_REPO_NAME/NeTEx $CONTENT_FOLDER
+
 rm -rf $CONTENT_FOLDER/SIRI
-
-echo "Cloning new SIRI content..."
-rm -rf $TEMP_FOLDER | true
-mkdir $TEMP_FOLDER && cd $TEMP_FOLDER
-git clone https://github.com/etalab/$SIRI_REPO_NAME.git
-cd $SIRI_REPO_NAME
-# just for debugging purposes
-git tag -l
-git checkout tags/$SIRI_VERSION
-cd ../..
-
-echo "Copying new SIRI content to the right place..."
+cloneContent https://github.com/etalab/$SIRI_REPO_NAME.git $SIRI_REPO_NAME $SIRI_VERSION
 cp -r $TEMP_FOLDER/$SIRI_REPO_NAME/SIRI $CONTENT_FOLDER
 
-echo "Building site..."
 hugo --minify
