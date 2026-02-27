80a03bdfa2d2619efd44fd9322293ed0bdb220f9
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-02-27 11:05:07 +0100

    Make it so we can publish a specific tag of NeTEx FR

--- build.sh
+++ build.sh
@@ -3,32 +3,44 @@
 # stop processing on basic errors
 set -e
 
-# NOTE: temporarily freezing to the latest tag,
+CONTENT_FOLDER=content/normes
+
+# NOTE: freezing to the tag matching the latest official release,
 # ultimately something more flexible with be introduced
 # (e.g. multi-version support, and first "automatic latest version")
 SIRI_VERSION=v1.7
-TEMP_FOLDER=tmp
-CONTENT_FOLDER=content/normes
+NETEX_VERSION=v2.3
+LOCAL_TEMP_FOLDER=./tmp
 SIRI_REPO_NAME=transport-profil-siri-fr
+NETEX_REPO_NAME=transport-profil-netex-fr
 
-echo "Retrieving content via git..."
-git submodule update --init --remote $CONTENT_FOLDER
+echo "Preparing local clones of repositories"
+rm -rf $LOCAL_TEMP_FOLDER
 
-echo "Removing legacy SIRI content..."
-rm -rf $CONTENT_FOLDER/SIRI
+function checkout_tag() {
+    local repo_name="$1"
+    local tag="$2"
+    echo "Checking out repo ${repo_name} at tag ${tag}"
+    git -c advice.detachedHead=false clone \
+        --depth 1 --single-branch \
+        --branch "${tag}" \
+        "https://github.com/etalab/${repo_name}.git" \
+        $LOCAL_TEMP_FOLDER/${repo_name}
+}
 
-echo "Cloning new SIRI content..."
-rm -rf $TEMP_FOLDER | true
-mkdir $TEMP_FOLDER && cd $TEMP_FOLDER
-git clone https://github.com/etalab/$SIRI_REPO_NAME.git
-cd $SIRI_REPO_NAME
-# just for debugging purposes
-git tag -l
-git checkout tags/$SIRI_VERSION
-cd ../..
+git submodule update --init themes/PaperMod
 
-echo "Copying new SIRI content to the right place..."
-cp -r $TEMP_FOLDER/$SIRI_REPO_NAME/SIRI $CONTENT_FOLDER
+checkout_tag "$NETEX_REPO_NAME" "$NETEX_VERSION"
+checkout_tag "$SIRI_REPO_NAME" "$SIRI_VERSION"
+
+echo "Removing legacy NeTEx & SIRI content..."
+rm -rf $CONTENT_FOLDER/SIRI $CONTENT_FOLDER/NeTEx
+
+echo "Copying cloned NeTEx content to the right place..."
+cp -r $LOCAL_TEMP_FOLDER/$NETEX_REPO_NAME/NeTEx $CONTENT_FOLDER
+
+echo "Copying cloned SIRI content to the right place..."
+cp -r $LOCAL_TEMP_FOLDER/$SIRI_REPO_NAME/SIRI $CONTENT_FOLDER
 
 echo "Building site..."
 hugo --minify
